7c14bafa6250dd785f80afd479b5d817dda8d826
Riley Hemphill <rileyhemp@gmail.com>
2020-03-30 15:10:47 -0400

    leaving projects expanded

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,26 +1,14 @@
 <template>
-	<div
-		class="project-wrapper"
-		:class="collapsed ? null : 'expanded'"
-		@click="
-			() => {
-				collapsed = !collapsed;
-			}
-		"
-	>
-		<section>
+	<article>
+		<img :src="project.image" alt="" />
+		<div class="project-description">
 			<h3>{{ project.name }}</h3>
 			<p class="body-1">{{ project.description }}</p>
-		</section>
-		<article :class="collapsed ? null : 'expanded'">
-			<img :src="project.image" alt="" />
-			<div class="project-description">
-				<p class="body-1">
-					{{ project.about }}
-				</p>
-			</div>
-		</article>
-	</div>
+			<p class="body-1">
+				{{ project.about }}
+			</p>
+		</div>
+	</article>
 </template>
 
 <script>
@@ -28,8 +16,7 @@ export default {
 	name: "Project",
 	data: function() {
 		return {
-			project: this.$attrs.project,
-			collapsed: true
+			project: this.$attrs.project
 		};
 	},
 	methods: {}
@@ -38,14 +25,12 @@ export default {
 
 <style scoped>
 article {
-	height: 0rem;
 	overflow: hidden;
 	display: flex;
 	flex-direction: row-reverse;
 	transition: all 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
-}
-article.expanded {
-	height: 20rem;
+	padding: 2.5rem 0 1.75rem 0;
+	border-top: 5px solid rgba(0, 0, 0, 0.2);
 }
 img {
 	max-width: 20rem;
@@ -54,5 +39,7 @@ img {
 }
 .project-description {
 	padding-right: 1rem;
+	display: flex;
+	flex-direction: column;
 }
 </style>
