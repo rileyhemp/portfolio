815b0df67a8d87c5f053b09912d1e695364ecc59
Riley Hemphill <rileyhemp@gmail.com>
2020-03-30 11:54:40 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="outer-wrapper">
+			<div class="inner-wrapper">
+				<nav>
+					<ul>
+						<a href="#"></a
+						>Projects
+					</ul>
+					<ul>
+						<a href="#"></a
+						>About
+					</ul>
+				</nav>
+				<header>
+					<h1>Hi, I'm Riley</h1>
+					<h2>I'm a frontend engineer for-hire</h2>
+					<p>I love writing web apps that provide practical solutions to real-world problems.</p>
+				</header>
+				<div class="spacer" />
+				<main>
+					<h2>Latest Projects</h2>
+				</main>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App"
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./css/main.scss";
 </style>
