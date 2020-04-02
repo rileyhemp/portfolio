5613dc9a7c0a77be2edb91678c43c5d55eae6d34
Riley Hemphill <rileyhemp@gmail.com>
2020-04-02 02:37:31 -0400

    Added bio

---
+++ src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<header>
+			<h1>Hi, I'm Riley</h1>
+		</header>
+		<div class="spacer" />
+		<main>
+			<p>I've been called a workaholic but it's not true. I just like to do things properly.</p>
+			<p>
+				I've been a copywriter, a professional fiddle player, a line cook, and a door-to-door salesman. I bake sourdough when I'm stressed, my
+				favorite beer is all of them, and I am a champion of the oxford comma.
+			</p>
+			<p>I currently live in Atlanta with my fiance and my dog, Rocco. He's a really good boy.</p>
+		</main>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "About"
+};
+</script>
+
+<style>
+i {
+	font-style: italic;
+}
+</style>
