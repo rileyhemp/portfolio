fbe5decd5adab19a60f4974a10381c9ced217706
Riley Hemphill <rileyhemp@gmail.com>
2020-04-02 02:56:13 -0400

    bio written

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,13 +4,18 @@
 			<h1>Hi, I'm Riley</h1>
 		</header>
 		<div class="spacer" />
-		<main>
-			<p>I've been called a workaholic but it's not true. I just like to do things properly.</p>
-			<p>
-				I've been a copywriter, a professional fiddle player, a line cook, and a door-to-door salesman. I bake sourdough when I'm stressed, my
-				favorite beer is all of them, and I am a champion of the oxford comma.
-			</p>
-			<p>I currently live in Atlanta with my fiance and my dog, Rocco. He's a really good boy.</p>
+		<main class="about">
+			<div>
+				<p>I've been called a workaholic but it's not true. I just like to do things properly.</p>
+				<p>
+					I've been a copywriter, a professional fiddle player, a line cook, and a door-to-door salesman. I bake sourdough when I'm
+					stressed, my favorite beer is all of them, and I'm a champion of the oxford comma.
+				</p>
+				<p>Currently living in Atlanta with my fiancée and dog, Rocco.</p>
+			</div>
+			<div class="image-container">
+				<img src="../../public/img/kat-and-riley.png" class="about-image" alt="Riley and Kat" />
+			</div>
 		</main>
 	</div>
 </template>
@@ -21,8 +26,23 @@ export default {
 };
 </script>
 
-<style>
-i {
-	font-style: italic;
+<style scoped>
+.about {
+	display: flex;
+	flex-direction: row;
+	width: 100%;
+	box-sizing: border-box;
+	align-items: center;
+}
+.image-container {
+	display: flex;
+	margin-left: 2rem;
+}
+.about-image {
+	width: 30vw;
+	height: 30vw;
+	object-fit: cover;
+	object-position: right;
+	border-radius: 50%;
 }
 </style>
